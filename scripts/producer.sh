afe203623a8700e1243202bd9def8275d11e300b
Struan <sclark@idtus.com>
2018-04-26 22:07:32 -0600

    changing producer to use hostnames

--- scripts/producer.sh
+++ scripts/producer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -gt 0 ]; then
-    $KAFKA_HOME/bin/kafka-console-producer.sh --topic "$1" --broker-list 10.30.3.10:9092,10.30.3.20:9092,10.30.3.30:9092
+    $KAFKA_HOME/bin/kafka-console-producer.sh --topic "$1" --broker-list vkc-br1:9092,vkc-br2:9092,vkc-br3:9092
 else
     echo "Usage: "$(basename $0)" <topic_name>"
 fi
