24bea8a08935dbda0ff9e3ab99aee62a50da3937
Struan <sclark@idtus.com>
2018-05-17 01:33:29 -0600

    fix port typo

--- scripts/describe-topics.sh
+++ scripts/describe-topics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$KAFKA_HOME/bin/kafka-topics.sh --describe --zookeeper vkc-zk1:2181,vkc-zk2:2181,vkc-zk3:2182
+$KAFKA_HOME/bin/kafka-topics.sh --describe --zookeeper vkc-zk1:2181,vkc-zk2:2181,vkc-zk3:2181
